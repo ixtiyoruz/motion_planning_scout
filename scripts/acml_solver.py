c51d62f7f1e0391ad2f7073b63287032f5663dad
ixtiyoruz <ixtiyoruz312@gmail.com>
2021-02-01 11:33:25 +0900

    ,

--- scripts/acml_solver.py
+++ scripts/acml_solver.py
@@ -2,33 +2,33 @@
 import rospy
 import ros_numpy
 import std_msgs.msg
+from geometry_msgs.msg import PoseWithCovarianceStamped
 import numpy as np
 import rospkg
 import subprocess
 import os
+from  std_srvs.srv import Empty
 # we have to save it like :: rosrun map_server map_saver -f map
 #known pump geometry
-class MapSaver:
+class LocalizationSolver:
     def __init__(self):
         rospy.init_node('amcl_solver_node')
         rospy.loginfo('amcl_solver_node node started')
         self.rospack = rospkg.RosPack()
-        self.sb_map = rospy.Subscriber("/map", OccupancyGrid, self.process_map)
-        # self.sb_map_meta = rospy.Subscriber("/map_metadata", MapMetaData, self.process_map_meta)
+        self.sb_pose = rospy.Subscriber("/amcl_pose", PoseWithCovarianceStamped, self.process_pose)
+        self.serive_global_localization = rospy.ServiceProxy('/global_localization', Empty)
         rospy.spin()
 
-    def process_map(self, message):        
-        print("map received", type(message))
+    def process_pose(self, message):        
+        # print("pose received", type(message.pose.covariance))
+        cov = np.array(message.pose.covariance)
         
-        # os.system("rosrun map_server map_saver -f " + self.save_folder)
-        test = subprocess.Popen("rosrun map_server map_saver -f " + self.save_folder, stdout=subprocess.PIPE, shell=True)
-        output = test.communicate()[0]
-        # print(message.header)
-        # print(message.info)
-        # print(len(message.data))
+        if(np.any(cov> 0.65)):
+            # invoke global_localization, with current covariance abd std
+            self.serive_global_localization()
+            print(np.max(cov))
+            pass
 
-    # def process_map_meta(self, message):
-    #     print("map_meta received", type(message))
-        
+
 if __name__ =='__main__':
-    map_saver = MapSaver()
+    map_saver = LocalizationSolver()
